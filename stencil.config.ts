a65185a13f9c2a7341c8e40f7c90e4723536939b
vitorhfs <vitorhugo.fernandes12@gmail.com>
2022-06-07 11:28:28 -0300

    feat(app): adds angular output to stencil config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget, ValueAccessorConfig } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'popover-dropdown',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: 'popover-dropdown',
+      directivesProxyFile: './angular-workspace/stencil-library/src/components.d.ts',
+      directivesArrayFile: './angular-workspace/stencil-library/src/index.ts'
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
