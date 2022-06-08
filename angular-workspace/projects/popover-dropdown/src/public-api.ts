636f6e051f81e6f4b81078ca98911dab93091a21
vitorhfs <vitorhugo.fernandes12@gmail.com>
2022-06-08 13:04:42 -0300

    feat: removes angular workspace from package

--- angular-workspace/projects/popover-dropdown/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of popover-dropdown
- */
-
-export * from './lib/popover-dropdown.service';
-export * from './lib/popover-dropdown.component';
-export * from './lib/popover-dropdown.module';
